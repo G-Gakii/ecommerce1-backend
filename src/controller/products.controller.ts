489a44d950dca0a869620c86381d65cd81b07ae5
“G-Gakii” <gakiiviolet1@gmail.com>
2024-12-13 13:06:30 +0300

    allow only owner to update product

--- src/controller/products.controller.ts
+++ src/controller/products.controller.ts
@@ -11,6 +11,7 @@ import {
   updateProductQuery,
 } from "../queries/products.queries";
 import { AuthorizedRequest } from "../interface/auth.interface";
+import { log } from "console";
 
 export const addProduct = async (req: AuthorizedRequest, res: Response) => {
   try {
@@ -74,12 +75,28 @@ export const getOneProduct = async (req: Request, res: Response) => {
   }
 };
 
-export const UpdateProduct = async (req: Request, res: Response) => {
+export const UpdateProduct = async (req: AuthorizedRequest, res: Response) => {
   try {
     const { id } = req.params;
+
+    if (!req.user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+    const userId = req.user.id;
     const { name, description, price, quantity } = req.body;
     if (!name && !description && !price && !quantity) {
       res.status(400).json({ message: "All fields required" });
+      return;
+    }
+    const product = await pool.query(getOneProductQuery, [id]);
+    if (product.rowCount === 0) {
+      res.status(404).json({ message: "Product not found" });
+      return;
+    }
+
+    if (product.rows[0].user_id !== req.user.id) {
+      res.status(401).json({ message: "Unauthorized" });
       return;
     }
 
@@ -88,12 +105,14 @@ export const UpdateProduct = async (req: Request, res: Response) => {
       description,
       price,
       quantity,
-      id,
+      userId,
     ]);
     res.status(200).json(updatedProduct.rows[0]);
+    return;
   } catch (error) {
     const err = error as Error;
     res.status(500).json({ message: `Internal server error ${err.message}` });
+    return;
   }
 };
 
