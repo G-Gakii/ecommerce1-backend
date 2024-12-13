eeb1ffd240c6ebb9e10f5a6cca77d0af18a5e79e
“G-Gakii” <gakiiviolet1@gmail.com>
2024-12-13 09:48:50 +0300

    authorization-only buyers can buy

--- src/router/cart.router.ts
+++ src/router/cart.router.ts
@@ -7,13 +7,29 @@ import {
   UpdateCartItem,
 } from "../controller/cart.controller";
 import autheticateUser from "../middleware/autheticate.middleware";
+import authorizedRole from "../middleware/authorization.middle";
 
 const router = Router();
 
-router.post("/cart", autheticateUser, addToCart);
-router.get("/cart", autheticateUser, getCartItem);
-router.put("/cart/:id", autheticateUser, UpdateCartItem);
-router.delete("/cart/:id", autheticateUser, DeleteItemFromCart);
-router.post("/cart/checkout", autheticateUser, checkOut);
+router.post("/cart", autheticateUser, authorizedRole(["buyer"]), addToCart);
+router.get("/cart", autheticateUser, authorizedRole(["buyer"]), getCartItem);
+router.put(
+  "/cart/:id",
+  autheticateUser,
+  authorizedRole(["buyer"]),
+  UpdateCartItem
+);
+router.delete(
+  "/cart/:id",
+  autheticateUser,
+  authorizedRole(["buyer"]),
+  DeleteItemFromCart
+);
+router.post(
+  "/cart/checkout",
+  autheticateUser,
+  authorizedRole(["buyer"]),
+  checkOut
+);
 
 export default router;
